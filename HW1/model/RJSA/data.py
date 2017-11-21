5aa1ba712afd2437d864c7ca65f5ea4acd354e84
gaof <gaof.student@gmail.com>
2017-11-22 00:12:17 +0800

    completegit add -A !

---
+++ HW1/model/RJSA/data.py
@@ -0,0 +1,19 @@
+import numpy as np
+import scipy.io as sio
+import matplotlib.pyplot as plt
+
+loss_val = np.load("valLoss2_Gauss.npy")[20:]
+loss_train = np.load("trainLoss2_Gauss.npy")[20:]
+
+L = len(loss_val)
+
+plt.figure()
+plt.plot(range(L), loss_val, "r", label = "loss_val")
+plt.plot(range(L), loss_train, "b", label = "loss_train")
+plt.legend()
+plt.savefig("newloss2.png")
+
+v1 = np.load("v1_Gauss.npy")
+v2 = np.load("v2_Gauss.npy")
+
+sio.savemat("RJMCMC2015011208.mat", {"v1": v1, "v2": v2})
